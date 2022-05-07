ad3f65d42a2aace1c46938928a049a2f6beaf5f8
lucaspires-source <pires.lucas94@gmail.com>
2022-05-07 12:40:27 -0300

    adding icons and close buttons into form

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,8 +1,45 @@
-export function WidgetForm (){
+import { CloseButton } from "./CloseButton";
 
+
+const feedbackTypes = {
+    BUG: {
+    title:'Problema',
+    image :{
+        src:'../../assets/icons/bug.svg',
+        alt:'problema'
+    }
+    },
+    IDEA:{
+        title:'Ideia',
+        image :{
+            src:'../../assets/icons/idea.svg',
+            alt:'ideia'
+        }
+        
+    },
+    OTHER :{
+        title:'Outro',
+        image :{
+            src:'../../assets/icons/other.svg',
+            alt:'outro'
+        }
+
+
+    }
+}
+export function WidgetForm (){
     return (    
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
-            Widget Form
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+                <CloseButton />
+            </header>
+            <div className="flex py-8 gap-2 w-full">
+                <button></button>
+            </div>
+            <footer className="text-xs text-neutral-400">
+                Feito por <a className="underline underline-offset-2" href="https://github.com/lucaspires-source">Lucas Ribeiro Pires</a>
+            </footer>
         </div>
 
 
