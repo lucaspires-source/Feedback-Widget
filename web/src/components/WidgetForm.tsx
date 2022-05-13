e50601336c8c86587e6bd351e270bf2a75b22d87
lucaspires-source <pires.lucas94@gmail.com>
2022-05-13 10:51:47 -0300

    started working on feedback buttons

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,27 +1,29 @@
 import { CloseButton } from "./CloseButton";
-
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import otherImageUrl from "../assets/other.svg";
 
 const feedbackTypes = {
     BUG: {
     title:'Problema',
     image :{
-        src:'../../assets/icons/bug.svg',
-        alt:'problema'
+        src:bugImageUrl,
+        alt:'Imagem de um inseto'
     }
     },
     IDEA:{
         title:'Ideia',
         image :{
-            src:'../../assets/icons/idea.svg',
-            alt:'ideia'
+            src:ideaImageUrl,
+            alt:'Imagem de uma lampada'
         }
         
     },
     OTHER :{
         title:'Outro',
         image :{
-            src:'../../assets/icons/other.svg',
-            alt:'outro'
+            src:otherImageUrl,
+            alt:'Imagem de um Balao de pensamento'
         }
 
 
@@ -32,10 +34,18 @@ export function WidgetForm (){
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
             <header>
                 <span className="text-xl leading-6">Deixe seu feedback</span>
+                
                 <CloseButton />
             </header>
             <div className="flex py-8 gap-2 w-full">
-                <button></button>
+                {Object.entries(feedbackTypes).map(([key,value])=>{
+                    return(
+                        <button className=" bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none" key={key}>
+                                <img className="h-12 w-12" src={value.image.src} alt={value.image.alt} />
+                                <span className="text-xl leading-6">{value.title}</span>
+                        </button>
+                    )
+                })}
             </div>
             <footer className="text-xs text-neutral-400">
                 Feito por <a className="underline underline-offset-2" href="https://github.com/lucaspires-source">Lucas Ribeiro Pires</a>
