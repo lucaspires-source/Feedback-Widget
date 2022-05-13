864cb1b5f79308e56d96ae22c6b5a8439eb4724a
lucaspires-source <pires.lucas94@gmail.com>
2022-05-13 15:42:45 -0300

    form initil structure

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -12,19 +12,35 @@ export default function FeedbackContentStep({feedbackType, onFeedbackReset} : Fe
     const feedbackTypeData = feedbackTypes[feedbackType]
   return (
     <React.Fragment>
-    <header>
-        <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100" onClick={onFeedbackReset}>
-            <ArrowLeft weight="bold" className="w-4 h-4"/>
-        </button>
-        <span className="text-xl leading-6 flex items-center gap-2">
-            <img src={feedbackTypeData.image.src} alt={feedbackTypeData.image.alt} />
-            {feedbackTypeData.title}      
-        </span>
-        <CloseButton />
-    </header>
-    <div className="flex py-8 gap-2 w-full">
+        <header>
+            <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100" onClick={onFeedbackReset}>
+                <ArrowLeft weight="bold" className="w-4 h-4"/>
+            </button>
+            <span className="text-xl leading-6 flex items-center gap-2">
+                <img src={feedbackTypeData.image.src} alt={feedbackTypeData.image.alt} className="w-6 h-6" />
+                {feedbackTypeData.title}      
+            </span>
+            <CloseButton />
+        </header>
+        <form className="my-4 w-full">
+            <textarea className="
+                    min-w-[304px] 
+                    w-full min-h=[112px] 
+                    text-sm 
+                    placeholder-zinc-400 
+                    text-zinc-100
+                    border-1 
+                    border-zinc-600 
+                    bg-transparent 
+                    rounded-md 
+                    focus:border-x-brand-500 
+                    focus:ring-brand-500 
+                    focus:ring-1 resize-none
+                    focus:outline-none
 
-    </div>
+                    " 
+                    placeholder="Conte com detalhes o que ocorreu"/>
+        </form>
     </React.Fragment>
   )
 }
