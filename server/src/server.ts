12f5bdcd736540a88a11d920149b0162cb4e8387
lucaspires-source <pires.lucas94@gmail.com>
2022-05-30 15:56:51 -0300

    prototype of post route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,23 @@
+import { prisma } from './prisma';
 import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3333;
 
-app.get('/users', (req, res) => {
-res.send('Hello World');
+app.use(express.json())
+
+app.post('/feedbacks', async (req, res) => {
+    const { type,comment,screenshot} = req.body;
+ const feedback = await prisma.feedback.create({
+        data:{
+             type,
+            comment,
+            screenshot,
+        }
+    })
+    return res.status(201).json({data:feedback})
 })
+
 app.listen(PORT, () =>{
     console.log(`Server started on http://localhost:${PORT}`);
 })
