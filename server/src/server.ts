ab43e8db4a8598953b2772c227ba4c46be0a90fb
lucaspires-source <pires.lucas94@gmail.com>
2022-06-05 07:57:05 -0300

    using cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { routes } from './routes';
+import cors from 'cors'
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
